dfa48628561e78c475d3aedac1827b0325f937f2
jupiro <aposti0225@yahoo.co.jp>
2021-06-13 10:01:10 +0900

    並列計算周り

--- CarRacing.cpp
+++ CarRacing.cpp
@@ -1,45 +1,61 @@
-// C++11
-#include <algorithm>
-#include <cstdlib>
 #include <iostream>
-#include <map>
-#include <sstream>
-#include <vector>
-#include <set>
 #include <string>
-
-using namespace std;
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <utility>
+#include <tuple>
+#include <cmath>
+#include <numeric>
+#include <set>
+#include <map>
+#include <array>
+#include <complex>
+#include <iomanip>
+#include <cassert>
+#include <random>
+#include <chrono>
+#include <valarray>
+#include <bitset>
+using ll = long long;
+using std::cin;
+using std::cout;
+using std::endl;
+// std::mt19937 rnd(std::chrono::steady_clock::now().time_since_epoch().count());
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+const int inf = (int)1e9 + 7;
+const long long INF = 1LL << 60;
 
 int main() 
 {
-  int N;
-  int K;
 
-  cin >> N;
+  int n, K;
+  cin >> n;
   cin >> K;
 
-  string out="";
-  string result;
-  getline(cin, result); // read the endline
+  std::string out="";
+  std::string result;
+  std::getline(cin, result); // read the endline
 
-  vector<int> id(N);
-  for (int i=0; i<N; i++) id[i]=i;
+  std::vector<int> id(n);
+  for (int i=0; i<n; i++) id[i]=i;
 
   //selection sort
-  for (int i=0; i<N; i++)
+  for (int i=0; i<n; i++)
   {
     int pos=i;      
-    for (int k=i+1; k<N; k++)
+    for (int k=i+1; k<n; k++)
     {       
-      string race=to_string(id[pos])+" "+to_string(id[k]);
+      std::string race=std::to_string(id[pos])+" "+std::to_string(id[k]);
       cout << race << endl;
       cout.flush();
 
-      getline(cin, result);
+      std::getline(cin, result);
       if (result[0]=='1') pos=k;
     }
-    out+=to_string(id[pos]);
-    if (i<N-1) out+=" ";      
+    out+=std::to_string(id[pos]);
+    if (i<n-1) out+=" ";      
 
     std::swap(id[i],id[pos]);
   }
