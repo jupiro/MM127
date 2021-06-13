67f249442a0f953b71862202d5c2891e320b52d7
jupiro <aposti0225@yahoo.co.jp>
2021-06-13 12:27:36 +0900

    クイックソート風に

--- CarRacing.cpp
+++ CarRacing.cpp
@@ -27,38 +27,121 @@ template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; }
 const int inf = (int)1e9 + 7;
 const long long INF = 1LL << 60;
 
-int main() 
+void ask(std::vector<int> &v, std::vector<int> &gomi)
 {
+  if((int)v.size() <= 1)
+    return;
+  auto tv = v;
+  gomi.clear();
+  for (int i = 0; i < (int)v.size(); ++i)
+  {
+    cout << v[i] << " \n"[i + 1 == (int)v.size()];
+  }
+  cout.flush();
+  std::string s;
+  std::getline(cin, s);
+  std::stringstream ss;
+  ss << s;
+  while(!ss.eof())
+  {
+    ss >> s;
+    gomi.emplace_back(stoi(s));
+  }
 
-  int n, K;
+  for (int i = 0; i < (int)gomi.size(); ++i)
+  {
+    v[gomi[i]] = tv[i];
+  }
+}
+int n, K, D;
+std::vector<int> gomi, tmp;
+void calc(std::vector<int> &v)
+{
+  if((int)v.size() <= K)
+  {
+    ask(v, gomi);
+    return;
+  }
+  tmp.clear();
+  for (int i = 0; i < K; ++i)
+  {
+    tmp.emplace_back(v[i]);
+  }
+  ask(tmp, gomi);
+  gomi.clear();
+  std::vector<std::vector<int>> vv(D + 1);
+  std::vector<int> s;
+  for (int i = 0; i < (int)tmp.size(); i++)
+  {
+    if(i % 2 == 0)
+      s.emplace_back(tmp[i]);
+    else
+      vv[(i + 1) / 2].emplace_back(tmp[i]);
+  }
+  tmp = s;
+  for (int i = K; i < (int)v.size(); ++i)
+  {
+    tmp.emplace_back(v[i]);
+    if(i + 1 == (int)v.size() or (int)tmp.size() == K)
+    {
+      ask(tmp,gomi);
+      int cur = 0;
+      for (const auto &e : tmp)
+      {
+        if(cur < (int)s.size() and e == s[cur])
+        {
+          cur += 1;
+        }
+        else
+        {
+          vv[cur].emplace_back(e);
+        }
+      }
+      tmp = s;
+    }
+  }
+  for (int i = 0; i < (int)vv.size(); ++i)
+  {
+    calc(vv[i]);
+  }
+  v.clear();
+  for (int i = 0; i < (int)vv.size(); ++i)
+  {
+    for(const auto &e : vv[i])
+    {
+      v.emplace_back(e);
+    }
+    if(i < (int)s.size())
+    {
+      v.emplace_back(s[i]);
+    }
+  }
+}
+void solve()
+{
   cin >> n;
   cin >> K;
-
-  std::string out="";
+  D = (K + 1) / 2;
   std::string result;
   std::getline(cin, result); // read the endline
 
-  std::vector<int> id(n);
-  for (int i=0; i<n; i++) id[i]=i;
-
-  //selection sort
-  for (int i=0; i<n; i++)
+  std::mt19937 rnd(89);
+  std::vector<int> order(n);
+  for (int i = 0; i < n; ++i)
   {
-    int pos=i;      
-    for (int k=i+1; k<n; k++)
-    {       
-      std::string race=std::to_string(id[pos])+" "+std::to_string(id[k]);
-      cout << race << endl;
-      cout.flush();
-
-      std::getline(cin, result);
-      if (result[0]=='1') pos=k;
-    }
-    out+=std::to_string(id[pos]);
-    if (i<n-1) out+=" ";      
-
-    std::swap(id[i],id[pos]);
+    order[i] = i;
   }
-  cout << out << endl;
+  std::shuffle(order.begin(), order.end(), rnd);
+  calc(order);
+  for (int i = 0; i < (int)order.size(); ++i)
+  {
+    cout << order[i] << " \n"[i + 1 == (int)order.size()];
+  }
   cout.flush();
+}
+int main() 
+{
+  solve();
+  return 0;
+
 }
